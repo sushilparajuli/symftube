4de206fc6f340a9ba1ef1b2cb94e762768e78cf6
Sushil Parajuli <sushilparajuli2010@gmail.com>
2020-07-20 15:16:00 +0400

    category and sub categories fixtures with twig template done

--- migrations/Version20200720104804.php
+++ migrations/Version20200720104804.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200713071017 extends AbstractMigration
+final class Version20200720104804 extends AbstractMigration
 {
     public function getDescription() : string
     {

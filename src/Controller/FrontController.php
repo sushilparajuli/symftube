dcfb30888194f397e0e1798b92c7de790bd1e021
Sushil Parajuli <sushilparajuli2010@gmail.com>
2020-07-20 18:26:37 +0400

    Abstract Category class some debugger setup
    - composer require symfony/var-dumper --dev  -> to make dump(var) work
    - composer require symfony/profiler-pack  --dev  -> to get profiler tools
    - composer require symfony/debug-bundle  --dev  -> to debug app easily

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Utils\CategoryTreeFrontPage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,8 +20,9 @@ class FrontController extends AbstractController
     /**
      * @Route("/video-list/category/{categoryname},{id}", name="video_list")
      */
-    public function videoList()
+    public function videoList($id, CategoryTreeFrontPage $categories)
     {
+        dump($categories);
         return $this->render('front/video_list.html.twig');
     }
 

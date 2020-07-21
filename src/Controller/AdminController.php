8c23b2c71813a9afad624ae9962ebe3285bdb581
Sushil Parajuli <sushilparajuli2010@gmail.com>
2020-07-21 15:26:09 +0400

    Categories dropdown list for admin area done

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Utils\CategoryTreeAdminList;
+use App\Utils\CategoryTreeAdminOptionList;
 use App\Entity\Category;
 
 /**
@@ -74,5 +75,13 @@ class AdminController extends AbstractController
     public function users()
     {
         return $this->render('admin/users.html.twig');
+    }
+
+    public function getAllCategories(CategoryTreeAdminOptionList $categories)
+    {
+        $categories->getCategoryList($categories->buildTree());
+        return $this->render('admin/_all_categories.html.twig', [
+            'categories' => $categories
+        ]);
     }
 }
